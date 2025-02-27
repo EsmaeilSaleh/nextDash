6068d4bcee3ab6e649cf7fc1811902ff439e7349
EsmaeilSaleh <e.saleh88@gmail.com>
2025-02-27 10:41:11 +0100

    add dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
           />
           <p className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a href="http://localhost:3000/dashboard" target="_blank" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.

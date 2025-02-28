fdd1d37f36c01b0f6fb1de6d002d454b3613d33c
EsmaeilSaleh <e.saleh88@gmail.com>
2025-02-28 11:46:07 +0100

    debounce added

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -39,7 +38,6 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    await new Promise((resolve) => setTimeout(resolve, 3000));
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
